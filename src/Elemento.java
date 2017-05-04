f7410f151eb2a0b3cadaa4809052a3f7875dedae
Juanda <Juanda@Colossus>
2017-05-04 16:33:13 -0500

    base

--- src/Elemento.java
+++ src/Elemento.java
@@ -1,3 +1,4 @@
+import processing.core.PApplet;
 
 public class Elemento {
 	
@@ -7,14 +8,53 @@ public class Elemento {
 	private float alto;
 	private int tipo;
 	
-	public Elemento(int mouseX, int mouseY, int alto, int ancho, int tipo) {
+	public Elemento(float xcreator, int y, int alto, int ancho, int tipo) {
 		
-		this.x = x;
+		this.x = xcreator;
 		this.y = y;
 		this.alto = alto;
 		this.ancho = ancho;
 		this.tipo = tipo;
 	
+	}
+	
+	
+	public void pintar(PApplet app) {
+
+		switch (tipo) {
+		
+		// PAJA
+		case 1:
+			app.fill(255,0,0);
+			app.ellipse(x, y, alto, ancho);
+			break;
+
+			// MADERA
+		case 2:
+			app.fill(0,0,255);
+			app.ellipse(x, y, alto, ancho);
+			break;
+			
+			//LADRILLO
+		case 3:
+			app.fill(0,255,0);
+			app.ellipse(x, y, alto, ancho);
+			break;
+		}
+		
+		
+		
+		
+		
+	}
+	
+	public void mover(PApplet app) {
+
+		
+		y+=4;
+		
+		ancho += 0.03;
+		alto += 0.03;
 	}
 
 }
--- src/Logica.java
+++ src/Logica.java
@@ -12,27 +12,55 @@ public class Logica {
 	private int pantallas;
 	private PApplet app;
 	private ArrayList <Elemento> elementos;
+	private float xcreator;
+	
+	
+	private int tipoelem;
+	
 	private PImage banda;
+	
+	private PImage fondoini, fondomenu, niveles;
 	
 	
 	
 	public Logica(PApplet app) {
+		
+		
 		this.app = app;
 	this.pantallas = 0;	
-	banda = app.loadImage("../data/banda.png");
+	this.tipoelem = 1;
+	this.xcreator = 600;
 	
-		
+	 // IMAGENES 
+	banda = app.loadImage("../data/banda.png");
+	fondoini = app.loadImage("../data/Pantallas-00-Intro.png");
+	fondomenu = app.loadImage("../data/Pantallas-00-Menu.png");	
+	
+	
+	// ARREGLOS
+	
+	elementos = new ArrayList<Elemento>();
 		
 	}
 
 	public void pintar(PApplet app) {
 
-		
+		app.smooth();
+		app.frameRate(60);
 		switch (pantallas) {
 		
 		// PANTALLA DE INICIO 
 		case 0:
 			
+			app.image(fondoini, 0, 0);
+			
+			
+			
+			break;
+ // INSTRUCCIONES
+		case 1:
+			
+			app.image(fondomenu, 0, 0);
 			app.fill(255,255,0);
 			app.rect(app.width/2-300, 550, 160, 40);
 			
@@ -47,18 +75,38 @@ public class Logica {
 			
 			
 			break;
- // INSTRUCCIONES
-		case 1:
-			
-		
-			
-			
-			break;
 			
 			// PANTALLA DE JUEGO 1  (JUEGO DE LA BANDA GUITAR HERO DONDE BAJAN LOS ELEMENTOS)
 		case 2:
 			app.image(banda, 0, 0, 1280, 720);
 			
+			
+			app.ellipse(xcreator, 125, 25, 25);
+			
+			
+			switch (tipoelem) {
+			case 1:
+				app.fill(255,0,0);
+				app.rect(300, 20, 100, 20);
+				break;
+
+			case 2:
+				app.fill(0,0,255);
+				app.rect(300, 20, 100, 20);
+				break;
+				
+			case 3:
+				app.fill(0,255,0);
+				app.rect(300, 20, 100, 20);
+				break;
+			}
+			
+			
+			for (int i = 0; i < elementos.size(); i++) {
+				
+				elementos.get(i).pintar(app);
+				elementos.get(i).mover(app);
+			}
 			
 			
 			
@@ -68,17 +116,24 @@ public class Logica {
 		
 		
 	}
+	
+	
+	
 
 	public void press() {
 		
-		if (app.mouseX>app.width/2+100 && app.mouseX<app.width/2+100+160 && app.mouseY >550 && app.mouseY <590 && pantallas == 0) {
+		// hacer click en instrucciones
 		
-			pantallas =1;
+		if (app.mouseX>app.width/2+100 && app.mouseX<app.width/2+100+160 && app.mouseY >550 && app.mouseY <590 && pantallas == 1) {
+		
+			pantallas =3;
 			
 		
 		}
 		
-		if (app.mouseX> app.width/2-250 && app.mouseX< app.width/2-250+160 &&  app.mouseY >550 && app.mouseY <590 && pantallas == 0 ) {
+		// hacer clic en jugar
+		
+		if (app.mouseX> app.width/2-250 && app.mouseX< app.width/2-250+160 &&  app.mouseY >550 && app.mouseY <590 && pantallas == 1) {
 			pantallas =2;
 		}
 
@@ -87,11 +142,56 @@ public class Logica {
 	}
 
 	public void key() {
+		
+		
+		
+		
+		if (app.keyCode == app.ENTER && pantallas ==0) {
+			
+			pantallas ++;
+			
+		}
+		
+		
+		if (app.keyCode == app.LEFT && xcreator>495) {
+			this.xcreator-=1*2;
+			
+		}
+		
+		
+		if (app.keyCode == app.RIGHT && xcreator<695) {
+			this.xcreator+=1*2;
+			
+		}
+		
+		if ( app.key == 'q' || app.key  == 'Q') {
+			tipoelem ++;
+		
+			if (tipoelem >3) {
+			tipoelem = 1;	
+				
+			}
+			
+			
+			
+		}
 
 		if ( app.key == 'a' || app.key  == 'A') {
 			
-			elementos.add(new Elemento(app.mouseX, app.mouseY, 50, 50,	(int) app.random(9)));
+			switch (tipoelem) {
+			case 1:
+				elementos.add(new Elemento(xcreator, 125, 25,25, 1));
 				
+				break;
+
+			case 2:
+				elementos.add(new Elemento(xcreator, 125, 25,25, 2));
+				break;
+				
+			case 3:
+				elementos.add(new Elemento(xcreator, 125, 25,25, 3));
+				break;
+			}
 			
 			
 		}
